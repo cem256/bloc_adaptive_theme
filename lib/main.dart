7e3d62666109f61d72e60f1a166a96b3c2c5e406
cem256 <cemo99@gmail.com>
2022-11-05 00:30:04 +0300

    Hydrated bloc version bumped to 9.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'utils/simple_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
@@ -5,20 +6,16 @@ import 'package:path_provider/path_provider.dart';
 
 import 'logic/theme_bloc.dart';
 import 'presentation/home_screen.dart';
-import 'utils/simple_observer.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final storage = await HydratedStorage.build(
+  Bloc.observer = SimpleObserver();
+  HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
   );
 
-  HydratedBlocOverrides.runZoned(
-    () => runApp(const BlocAdaptiveTheme()),
-    storage: storage,
-    blocObserver: SimpleObserver(),
-  );
+  runApp(const BlocAdaptiveTheme());
 }
 
 class BlocAdaptiveTheme extends StatelessWidget {
